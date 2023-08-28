ae33647d145857e560f598a2334a543e881f43cc
Kye Nelson <kyenelson3@gmail.com>
2023-08-27 22:37:28 -0700

    Added styling for sprite renderer component

--- Locus/src/Locus/Renderer/Texture.h
+++ Locus/src/Locus/Renderer/Texture.h
@@ -16,7 +16,8 @@ namespace Locus
 		virtual uint32_t GetHeight() const = 0;
 		virtual uint32_t GetRendererID() const = 0;
 
-		virtual const std::string GetTexturePath() const = 0;
+		virtual const std::string& GetTexturePath() const = 0;
+		virtual const std::string GetTextureName() const = 0;
 
 		virtual void SetData(void* data, uint32_t size) = 0;
 

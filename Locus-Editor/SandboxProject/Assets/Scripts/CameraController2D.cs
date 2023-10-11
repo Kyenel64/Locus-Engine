6249a7609e410aed0d22491306ee04f3b6f11236
Kye Nelson <kyenelson3@gmail.com>
2023-10-11 17:44:44 +0900

    Moved texture file to resources folder

---
+++ Locus-Editor/SandboxProject/Assets/Scripts/CameraController2D.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Locus;
+
+namespace Sandbox
+{
+    // Sample class to demonstrate C# API
+    public class CameraController2D : Entity
+    {
+        void OnCreate()
+        {
+            
+        }
+
+        void OnUpdate(float deltaTime)
+        {
+            float speed = 5.0f;
+            Vec3 velocity = Vec3.Zero;
+
+            if (Input.IsKeyPressed(KeyCode.W))
+                velocity.y = 1.0f;
+            else if (Input.IsKeyPressed(KeyCode.S))
+                velocity.y = -1.0f;
+
+            if (Input.IsKeyPressed(KeyCode.A))
+                velocity.x = -1.0f;
+            else if (Input.IsKeyPressed(KeyCode.D))
+                velocity.x = 1.0f;
+
+            if (Input.IsKeyPressed(KeyCode.Up))
+                velocity.z = -1.0f;
+            else if (Input.IsKeyPressed(KeyCode.Down))
+                velocity.z = 1.0f;
+
+            velocity *= speed;
+            Transform.Position += velocity * deltaTime;
+
+        }
+    }
+}
